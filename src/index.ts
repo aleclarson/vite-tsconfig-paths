c43ce5ac17236d70cb9d1b012f5bfb35642af1f0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-08-05 20:12:01 -0400

    chore: add debug logs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { relative } from 'path'
 import { createMatchPath, loadConfig } from 'tsconfig-paths'
 import { mainFields, supportedExts, Resolver } from 'vite/dist/node/resolver'
 
+const debug = require('debug')('vite-tsconfig-paths')
+
 let resolver: Resolver = {}
 
 const config = loadConfig()
@@ -17,7 +19,11 @@ if (config.resultType == 'failed') {
   resolver = {
     alias(id) {
       let path = matchPath(id, undefined, undefined, supportedExts)
-      return path ? '/' + relative(process.cwd(), path) : undefined
+      if (path) {
+        path = '/' + relative(process.cwd(), path)
+        debug(`resolved "${id}" to "${path}"`)
+      }
+      return path
     },
   }
 }
