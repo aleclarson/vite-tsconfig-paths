7849290e64ddbf1bc0334e875dddc0c4213d7dce
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-20 11:38:48 -0400

    test: force `os.platform` to return win32

--- spec/setup.ts
+++ spec/setup.ts
@@ -7,3 +7,11 @@ jest.mock('tsconfig-paths', () => ({
 jest.mock('../src/config', () => ({
   loadConfig,
 }))
+
+jest.mock('os', () => ({
+  ...jest.requireActual('os'),
+  // Since the "path.win32" module supports both /^\// and /^[A-Z]:[\\/]/
+  // path formats, we can use it to test windows-style paths without
+  // breaking the tests for posix-style paths.
+  platform: () => 'win32',
+}))
