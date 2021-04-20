62a3c0953f6225e18077f8d34646602103ccd2e4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-20 16:14:42 -0400

    test: add tests for include array

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,3 +1,4 @@
+import { configs } from './mocks'
 import { getResolver, viteResolve } from './resolver'
 
 describe('tsconfig precedence', () => {
@@ -40,5 +41,66 @@ describe('absolute paths in projects array', () => {
     viteResolve.mockImplementation((id) => id)
     const result = await resolveId('c', 'D:\\a\\b\\b\\main.ts')
     expect(result).toMatchInlineSnapshot(`"D:/a/b/c"`)
+  })
+})
+
+describe('include array', () => {
+  it('can include files outside the config root', async () => {
+    configs['/a/b/']!.include = ['../x']
+    const resolveId = getResolver({
+      projects: ['/a/b'],
+    })
+    viteResolve.mockImplementation((id) => id)
+    const result = await resolveId('c', '/a/x/main.ts')
+    expect(result).toBeDefined()
+    expect(result).toMatchInlineSnapshot(`"/a/b/c"`)
+  })
+
+  it('can contain a path prefixed with ./', async () => {
+    configs['/a/']!.include = ['./src']
+    const resolveId = getResolver({
+      projects: ['/a'],
+    })
+
+    viteResolve.mockImplementation((id) => id)
+    let result = await resolveId('c', '/a/src/main.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+
+    // All descendants are matched.
+    result = await resolveId('c', '/a/src/b/main.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+  })
+
+  it('can contain a path with no prefix and no glob', async () => {
+    configs['/a/']!.include = ['src']
+    const resolveId = getResolver({
+      projects: ['/a'],
+    })
+
+    viteResolve.mockImplementation((id) => id)
+    let result = await resolveId('c', '/a/src/b.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+
+    // All descendants are matched.
+    result = await resolveId('c', '/a/src/b/index.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+  })
+
+  it('can contain a glob', async () => {
+    configs['/a/']!.include = ['src/**/__tests__']
+    const resolveId = getResolver({
+      projects: ['/a'],
+    })
+
+    viteResolve.mockImplementation((id) => id)
+    let result = await resolveId('c', '/a/src/__tests__/x.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+
+    result = await resolveId('c', '/a/src/b/__tests__/x.ts')
+    expect(result).toMatchInlineSnapshot(`"/a/c"`)
+
+    // This importer is not included.
+    result = await resolveId('c', '/a/src/main.ts')
+    expect(result).toMatchInlineSnapshot(`undefined`)
   })
 })
