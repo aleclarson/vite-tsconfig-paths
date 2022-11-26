2b267482d1c958b5d544b0be4e93bfa10f36361f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-11-26 00:54:00 -0500

    chore: update demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,5 +3,17 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths({
+      parseNative: false,
+    }),
+    // {
+    //   name: 'measure-startup-time-and-quit',
+    //   resolveId() {
+    //     console.log('Startup time:', process.uptime() + 's')
+    //     process.exit()
+    //   },
+    // },
+  ],
 })
