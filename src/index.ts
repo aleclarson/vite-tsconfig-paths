7ac675a562e2f0182e5449c9497c5c78e3c953cf
Igor Dreher <igor.dreher@gmail.com>
2023-04-10 13:00:41 -0300

    fix: check for undefined "compilerOptions" property (#108)
    Co-authored-by: Alec Larson <1925840+aleclarson@users.noreply.github.com>
    
    Closes #93

--- src/index.ts
+++ src/index.ts
@@ -151,7 +151,7 @@ export default (opts: PluginOptions = {}): Plugin => {
       files?: string[]
       include?: string[]
       exclude?: string[]
-      compilerOptions: CompilerOptions
+      compilerOptions?: CompilerOptions
     }
 
     debug('config loaded:', inspect({ configPath, config }, false, 10, true))
@@ -167,7 +167,7 @@ export default (opts: PluginOptions = {}): Plugin => {
       return null
     }
 
-    const options = config.compilerOptions
+    const options = config.compilerOptions || {}
     const { baseUrl, paths } = options
     if (!baseUrl && !paths) {
       debug(`[!] missing baseUrl and paths: "${configPath}"`)
