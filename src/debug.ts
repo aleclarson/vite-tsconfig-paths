0dc9a8b9b0ec8657f9d6980ac33d0bc090e278ef
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-11-19 15:10:27 -0500

    chore: ensure debug logs can be enabled in vitest

--- src/debug.ts
+++ src/debug.ts
@@ -2,3 +2,7 @@ import createDebug from 'debug'
 
 export const debug = createDebug('vite-tsconfig-paths')
 export const debugResolve = createDebug('vite-tsconfig-paths:resolve')
+
+if (process.env.TEST) {
+  createDebug.log = console.log.bind(console)
+}
