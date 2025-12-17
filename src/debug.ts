b9a5a813b825c0346c17e57ccc82539e68ecc3a1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-12-16 21:47:41 -0500

    chore(test): enable debug logs in test suite

--- src/debug.ts
+++ src/debug.ts
@@ -3,5 +3,5 @@ import createDebug from 'debug'
 export const debug = createDebug('vite-tsconfig-paths')
 
 if (process.env.TEST === 'vite-tsconfig-paths') {
-  createDebug.log = console.log.bind(console)
+  createDebug.enable('vite-tsconfig-paths')
 }
